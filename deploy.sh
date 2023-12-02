f5f80e33274376f7dc6309475311e43e03264e0d
HaixingOoO <974758671@qq.com>
2023-12-02 13:14:45 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-git push -f https://github.com/Zzongke/blog.git master
+git push -f https://github.com/HaixingOoO/blog.git master
 
 cd -
